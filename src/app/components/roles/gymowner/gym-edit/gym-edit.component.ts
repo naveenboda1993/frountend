b69e3f080008b320b7134769de87931a0cc1f907
naveenboda19 <naveenboda@infinityspark.com>
2020-01-14 12:44:24 +0530

    gym edit commit

---
+++ src/app/components/roles/gymowner/gym-edit/gym-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gym-edit',
+  templateUrl: './gym-edit.component.html',
+  styleUrls: ['./gym-edit.component.css']
+})
+export class GymEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
